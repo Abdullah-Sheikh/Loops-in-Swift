882bb7daec7d4696ae058c55075f189482b3074d
Abdullah Sheikh <sheikhabdullahkurana@gmail.com>
2020-11-26 11:41:45 +0500

    initial commit

---
+++ loop.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var num = 0
+// while loop .....
+while num < 10
+{
+    num=num+1
+    
+}
+
+
+// for loop .....
+for number in 0...10
+{
+    number
+}
+
+
+for number in [2,4,7,9]
+{
+    number
+}
+
